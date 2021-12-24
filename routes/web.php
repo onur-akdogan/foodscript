0b92450b2c122a2f24ae97d4d2529f7edd4a6ff9
onur-akdogan <onurakdogan7106@gmail.com>
2021-12-24 10:05:16 +0300

    step 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dashboard', function () {
+    return view('admin.index');
+});
 Route::get('/', function () {
     return view('welcome');
 });
